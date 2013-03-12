6186dfa50df21797cd2bde38e753e12489099ccb
mattn <mattn.jp@gmail.com>
2013-03-12 12:23:42 +0900

    First import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MattnCloud66::Application.config.secret_token = '2dbf19bfa102d344e5c49119212df2504d3ec08357161ba33956448c4fdb67ab8d87b183c11a51c7e170586263684affb65a4a17fb12ff64c8629630760bc5b7'
